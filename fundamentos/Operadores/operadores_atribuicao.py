2fdde1c70de3808ea4e69dfb5ce3efcca7dbec14
Pedro Souza <86263872+pesouzaaa@users.noreply.github.com>
2026-03-03 06:11:29 -0300

    add Fundamentos_pt2

---
+++ fundamentos/Operadores/operadores_atribuicao.py
@@ -0,0 +1,23 @@
+saldo = 500 #Atribuicao Simples
+print(saldo)
+
+saldo += 40 #Atribuicao Adicao
+print(saldo)
+
+saldo -= 30 #Atribuicao Subtracao
+print(saldo)
+
+saldo *= 5
+print(saldo) #Atribuicao Multiplicacao
+
+saldo /= 3 #Atribuicao Divisao
+print(saldo)
+
+saldo //= 2 #Atribuicao Divisao Inteira
+print(saldo)
+
+saldo %= 10 #Atribuicao Modulo
+print(saldo)
+
+saldo **= 5 #Atribuicao Exponenciacao
+print(saldo)
