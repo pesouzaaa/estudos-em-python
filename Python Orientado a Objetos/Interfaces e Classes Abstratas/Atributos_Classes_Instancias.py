b6a4b2379b425f3c0aab5939f22af05fc033213f
pesouazaaa <pedrohenriquedealvessoua@gmail.com>
2026-03-23 07:52:13 -0300

    add Python_POO_pt6

---
+++ Python Orientado a Objetos/Interfaces e Classes Abstratas/Atributos_Classes_Instancias.py
@@ -0,0 +1,22 @@
+class Estudante:
+    escola = "DevLune" #Atributo de Classe
+
+    def __init__(self, nome, numero): #Atributo de Instancia
+        self.nome = nome 
+        self.numero = numero
+
+    
+    def __str__(self):
+        return f"{self.nome} ({self.numero}) - {self.escola}"
+    
+def mostrar_valores(*objs):
+    for obj in objs:
+        print(obj)
+    
+pe = Estudante("Pedro", 609843)
+babi = Estudante("Barbara", 8209383)
+
+mostrar_valores(pe, babi)
+
+print(babi)
+print(pe)
