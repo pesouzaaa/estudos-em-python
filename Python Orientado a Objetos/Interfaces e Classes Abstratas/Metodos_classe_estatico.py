f4a27074ef445715fa286470ca8c53be7e58dd19
pesouazaaa <pedrohenriquedealvessoua@gmail.com>
2026-03-26 07:33:09 -0300

    add Python_POO_pt7

--- Python Orientado a Objetos/Interfaces e Classes Abstratas/Metodos_classe_estatico.py
+++ Python Orientado a Objetos/Interfaces e Classes Abstratas/Metodos_classe_estatico.py
@@ -1,11 +1,22 @@
 class Pessoa:
-    def __init__(self, nome, idade):
+    def __init__(self, nome=None, idade=None):
         self.nome = nome
         self.idade = idade 
 
-    def criar_apartir_data_nascimento(self, ano, mes, dia, nome):
+    @classmethod #cria o metodo classe
+    def criar_apartir_data_nascimento(cls, ano, mes, dia, nome):
         idade = 2026 - ano
-        return Pessoa(nome, idade)
+        return cls(nome, idade) #ao inves de colocar return Pessoa - usa o cls
 
-p = Pessoa("Pedro", 20)
-print(p.nome, p.idade)
+    @staticmethod # cria  o metodo estatico
+    def e_maior_idade(idade):
+        return idade >= 18
+
+# p = Pessoa("Pedro", 20)
+# print(p.nome, p.idade)
+
+p2 = Pessoa.criar_apartir_data_nascimento(2005, 7, 12, "Pedro") # Para chamar o método, nao precisa colocar "()" - basta chama-lo no ponto
+print(p2.nome, p2.idade)
+
+print(Pessoa.e_maior_idade(18))
+print(Pessoa.e_maior_idade(8))
