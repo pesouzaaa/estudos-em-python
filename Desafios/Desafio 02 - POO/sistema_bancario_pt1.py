250bea3d5b41699c942e794ef26cebb959dd82b7
pesouazaaa <pedrohenriquedealvessoua@gmail.com>
2026-04-14 07:32:46 -0300

    add Python_DesafioPOO_pt3

--- Desafios/Desafio 02 - POO/sistema_bancario_pt1.py
+++ Desafios/Desafio 02 - POO/sistema_bancario_pt1.py
@@ -35,10 +35,61 @@ class  Conta:
     @property
     def saldo (self):
         return self._saldo
+    
+    @property
+    def numero (self):
+        return self._numero
 
-   
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
+    
+    def sacar(self, valor):
+       saldo = self.saldo
+       excedeu_saldo = valor > saldo
+
+       if excedeu_saldo:
+           print("\n@@@ Operacao falhou! Voce nao tem saldo suficiente. @@@")
+       
+       elif valor > 0:
+           self._saldo -= valor
+           print("\n### Saque realizado com sucesso! ###")
+           return True
+
+       else: 
+           print("\n@@@ A operacao falhou! O valor informado é invalido. @@@") 
+       
+       return False
+    
+    def depositar(self, valor):
+        if valor > 0:
+            self._saldo += valor
+            print("\n#### Deposito realizado om sucesso! ###")
+        else:
+            print("\n@@@ Operacao falhou! O valor informado é inválido. @@@")
+            return False
+        
+        return True
+
 class  ContaCorrente(Conta):
-    pass
+    def __init__(self, numero, cliente, limite=500, limite_saques=3):
+        super().__init__(numero, cliente)
+        self.limite = limite
+        self.limite_saques = limite_saques
+
+    def sacar(self, valor):
+        numero_saques = len(
+            (transacao for transacao in self.historico.transacoes if transacao ["tipo"]== Saque.__name__)
+        )
+#06:02
 
 class  Historico:
     pass
